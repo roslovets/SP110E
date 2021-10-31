e5078a831e3b909868b95c6f663f4f63c4ebe2bf
Pavel Roslovets <p.v.roslovets@gmail.com>
2021-11-01 02:25:45 +0300

    doc(examples): switch_on example

---
+++ examples/switch_on.py
@@ -0,0 +1,13 @@
+import asyncio
+from src.SP110E.Controller import Controller
+
+
+async def main():
+    device = Controller()
+    await device.connect('AF:00:10:01:C8:AF')
+    await device.switch_on()
+    await device.set_brightness(255)
+    await device.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
