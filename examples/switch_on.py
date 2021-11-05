a9d8e21679f630fecdddcb166d7c59ff73e17cc2
Pavel Roslovets <p.v.roslovets@gmail.com>
2021-11-05 14:28:09 +0300

    fix: example

--- examples/switch_on.py
+++ examples/switch_on.py
@@ -4,7 +4,7 @@ from SP110E.Controller import Controller
 
 async def main():
     device = Controller()
-    await device.connect('AF:00:10:01:C8:A1')
+    await device.connect('AF:00:10:01:C8:AF')
     await device.switch_on()
     await device.set_brightness(255)
     await device.disconnect()
