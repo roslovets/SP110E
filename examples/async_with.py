0acc4c3c7a62cea84017fa3f96d5c91ea91c4e3a
Pavel Roslovets <p.v.roslovets@gmail.com>
2021-11-07 03:47:19 +0300

    doc: update examples

---
+++ examples/async_with.py
@@ -0,0 +1,16 @@
+import asyncio
+import random
+from SP110E.Controller import Controller
+
+
+async def main():
+    async with Controller('AF:00:10:01:C8:AF', timeout=2, retries=1) as device:
+        await device.switch_on()
+        await device.set_brightness(255)
+        for i in range(0, 10):
+            color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+            await device.set_color(color)
+        await device.switch_off()
+
+if __name__ == "__main__":
+    asyncio.run(main())
